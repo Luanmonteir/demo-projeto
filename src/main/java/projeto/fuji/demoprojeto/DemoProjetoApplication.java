44d98b7f58f3ca063a89baac6a034e5a1a843679
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2023-11-27 23:20:12 -0300

    consulta

---
+++ src/main/java/projeto/fuji/demoprojeto/DemoProjetoApplication.java
@@ -0,0 +1,13 @@
+package projeto.fuji.demoprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjetoApplication.class, args);
+	}
+
+}
